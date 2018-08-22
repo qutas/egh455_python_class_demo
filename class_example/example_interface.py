e51220a9c18f3e9fec954df201ba6a7389af147c
pryre <kyemorton@hotmail.com>
2018-08-23 09:50:36 +1000

    upload

---
+++ class_example/example_interface.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+class ExampleInterface():
+	a = float()
+	a = str()
+
+	def __init__(self, a_in, b_in):
+		self.a = float(a_in)
+		self.b = str(a_in)
--- python_class_demo.py
+++ python_class_demo.py
@@ -2,10 +2,23 @@
 
 from class_example.class_example import ClassExample
 from class_example.other_class_example import OtherClassExample
+from class_example.example_interface import ExampleInterface
 
-interface = ClassExample()
-other_interface = OtherClassExample()
+print( "--- Class Example ---")
 
-print( "ClassExample->process_val(2): %d" % interface.process_val(2) )
-print( "ClassExample->process_other_val(2): %d" % interface.process_other_val(2) )
-print( "OtherClassExample->process_val(2): %d" % other_interface.process_val(2) )
+class_a = ClassExample()
+class_b = OtherClassExample()
+
+print( "ClassExample->process_val(2): %d" % class_a.process_val(2) )
+print( "ClassExample->process_other_val(2): %d" % class_a.process_other_val(2) )
+print( "OtherClassExample->process_val(2): %d" % class_b.process_val(2) )
+
+print( "--- Class as Interface ---")
+
+a = 5
+b = 1
+
+interface = ExampleInterface(a, b)
+
+print("InterfaceExample->a: (%s) %s" % (type(interface.a).__name__, str(interface.a)) )
+print("InterfaceExample->b: (%s) %s" % (type(interface.b).__name__, str(interface.b)) )
